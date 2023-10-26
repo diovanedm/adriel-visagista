165b5c7147508fa18ce1006a352a44fd1e92e5db
diovanedm <diovane.dm@gmail.com>
2023-10-26 20:20:06 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,55 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <div className='flex flex-col gap-4'>
+      <h1 className='text-center font-bold text-xl'>Adriel Visagista</h1>
+
+      <div>
+        <h1 className='text-lg font-bold '>O que é a analise visagista</h1>
+        <p>
+          O visagismo é Customização, não é padronização. Não há certo ou errado, há tão e somente aquilo que “você” deseja expressar, seu eu real visto através de seu rosto e todos os elementos que ali estão. Por que fazer essa análise? A imagem comunica muito mais do que as palavras. As linhas, formas e cores dos cabelos, sobrancelhas, maquiagem, dentes, roupas e outros elementos visuais, interagem com as do rosto e do corpo, criando uma imagem pessoal completa e diversas reações emocionais. Em nosso “senso de identidade” ( senso de uma pessoa sobre ela mesma ), nosso corpo representa apenas 30% e o rosto 70%. Por isso, é tão importante uma consultoria visagista e conseguir transformar todos os elementos desse rosto, em um reflexo do seu verdadeiro eu, que a deixe segura e expresse sua essência.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+
+
+      <div>
+        <h1 className='text-lg font-bold '>Objetivos da análise visagista</h1>
+        <p>
+          Adequar a imagem pessoal do cliente à um estilo personalizado, que reflita sua beleza interior e que o ajude a alcance objetivos de imagem.
+
+          Mas, como funciona a Consultoria Marcia? No meu caso, ela tem esse formato:
+        </p>
+      </div>
+
+      <div>
+        <h1 className='text-lg font-bold '>Primeira parte da consultoria</h1>
+        <p>
+          Análise do rosto e suas medidas . Através dessas medidas, iremos descobrir o que significa cada detalhe do seu rosto, pela análise das proporções do terço superior, médio e inferior ( regras dos terços ). Também faremos análise de “energia”, para saber se é céu, terra ou losango e de temperamentos, que mostrará sua personalidade.         Análise de estilo de vida e Estilo Pessoal. Teste de Estilo         Teste gestual ( para entender quais seus desejos em relação ao corte de cabelo )         Análise das exigências profissionais         Análise das necessidades pessoais, desejos e preferência         Colorimetria ( somente presencial ) – Análise cromática, para saber temperatura da pele e se é classificada como Primavera, verão, outono ou inverno. Esse será o termômetro para indicação de cor de maquiagem e cor de cabelo. ( A cartela de cor pessoal não está sendo realizada nesse momento de pandemia e utilizamos outros 5 pilares de cores para consultoria, como cor de essência, cor de alma, psicologia das cores, harmonias de cores e contraste )
+        </p>
+      </div>
+
+      <div>
+        <h1 className='text-lg font-bold '>Segunda parte da consultoria</h1>
+        <p>
+          Esse segundo encontro acontecerá 1 semana após o primeiro. Será entregue o Dossiê em formato PDF ( que poderá imprimi-lo e encaderná-lo se quiser ), com todas as informações a respeito de seu rosto e as sugestões para você alcançar seu objetivo de imagem pessoal, através do rosto e todos os elementos que o envolvem ( Cabelo, maquiagem, sobrancelhas e acessórios ). Em seu Dossiê Visagista, estará:
+        </p>
+
+        <div className='flex flex-col gap-3 mt-2'>
+          <p>1. Análise geral do rosto (  como acentuar caráter ou harmonizar )</p>
+          <p>          2. Energias ( qual é sua energia , seu significado, como acentuar caráter ou harmonizar )
+          </p>
+          <p>3. Temperamentos ( quais são ou qual é, seu significado, como trabalhar temperamentos, através do imagem do rosto )</p>
+          <p>4. Análise do contraste ( diferença entre cor de cabelo, pele, sobrancelhas )</p>
+          <p>5. Cores de maquiagem e cores de cabelo, para harmonizar com tipo de pele ( quente, fria ou neutra )</p>
+          <p>6. Sugestões de cortes de cabelo e penteados, segundo objetivos de imagem, considerando se você quer acentuar ( manter características ) ou equilibrar caráter (harmonizar as formas). Sugestões de profissionais para corte, cor e penteado, se necessário.</p>
+          <p>7. Sugestão de tipos de óculos e brincos, para harmonizar ou acentuar características também.</p>
+          <p>8. Sugestões de procedimentos estéticos ( rosto e dentes ), se necessário e indicações de profissionais.</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
